a1f27423d428d9035c0537aa1c2e764663660828
Joe Flood <joseph.flood@hotmail.com>
2013-03-13 09:17:32 +1100

    added missing files

---
+++ IEventSourcedMyBrain/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IEventSourcedMyBrain.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+
+    public class LiveController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
