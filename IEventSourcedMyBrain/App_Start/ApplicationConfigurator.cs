fc6aa7ada73d73b172d6433f86575941900f2693
Joe Flood <joseph.flood@hotmail.com>
2013-03-13 16:56:44 +1100

    adding autofac again

---
+++ IEventSourcedMyBrain/App_Start/ApplicationConfigurator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Reflection;
+using System.Web.Http;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Core;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using IEventSourcedMyBrain.Controllers;
+
+namespace IEventSourcedMyBrain
+{
+    public static class ApplicationConfigurator
+    {
+        public static void Configure(HttpConfiguration cfg)
+        {
+            var builder = new ContainerBuilder();
+
+            builder.Register(ctx => EventStoreConnectionFactory.Create())
+                   .SingleInstance();
+            builder.RegisterType<EventStoreReader>()
+                   .AsSelf().InstancePerApiRequest();
+
+            builder.RegisterType<RelayService>()
+                   .WithParameter(
+                    new TypedParameter(typeof (Func<Uri, Uri>), 
+                    new Func<Uri, Uri>(uri => RelayUri(uri))))
+                    .InstancePerHttpRequest();
+
+            builder.RegisterType<LinkInterceptor>().AsSelf()
+                   .WithParameter(new NamedParameter("host", Config.Host))
+                   .WithParameter(new NamedParameter("port", Config.Port))
+                   .WithParameter(new TypedParameter(typeof (HttpConfiguration), cfg))
+                   .InstancePerHttpRequest();
+
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+            cfg.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+
+
+        private static Uri RelayUri(Uri requestUri)
+        {
+            var ub = new UriBuilder(requestUri);
+            ub.Host = Config.Host;
+            ub.Port = Config.Port;
+            ub.Query = "format=json";
+            return ub.Uri;
+        }
+    }
+}
---
+++ IEventSourcedMyBrain/App_Start/EventStoreConnectionFactory.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+using EventStore.ClientAPI;
+
+namespace IEventSourcedMyBrain
+{
+    public class EventStoreConnectionFactory
+    {
+        public static EventStoreConnection Create()
+        {
+            var connection = EventStoreConnection.Create();
+            connection.Connect(new IPEndPoint(IPAddress.Parse(Config.Host), 1113));
+            return connection;
+        }
+    }
+}
